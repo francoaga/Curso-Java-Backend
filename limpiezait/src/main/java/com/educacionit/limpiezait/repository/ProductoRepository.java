b844160cf83e7e80f98f9ba3ffcabd7ce2e52575
francoaga <fandresgroselle@gmail.com>
2025-03-12 15:14:34 -0300

    Clase ProductoRepository que implementara la interfaz IProductoRepository

---
+++ limpiezait/src/main/java/com/educacionit/limpiezait/repository/ProductoRepository.java
@@ -0,0 +1,32 @@
+package com.educacionit.limpiezait.repository;
+
+import com.educacionit.limpiezait.model.Producto;
+import com.educacionit.limpiezait.repository.interfaces.IProductoRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class ProductoRepository implements IProductoRepository {
+    private final List<Producto> personas = new ArrayList<>();
+
+    @Override
+    public List<Producto> findAll() {
+        return List.of();
+    }
+
+    @Override
+    public Optional<Producto> findBy(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Producto save(Producto entity) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+}
