14e14e416baf9c4904111f5a78268539554f2f83
francoaga <fandresgroselle@gmail.com>
2025-03-12 14:13:36 -0300

    Inicializando el proyecto de Spring Boot

---
+++ limpiezait/src/main/java/com/educacionit/limpiezait/LimpiezaitApplication.java
@@ -0,0 +1,13 @@
+package com.educacionit.limpiezait;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimpiezaitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimpiezaitApplication.class, args);
+	}
+
+}
