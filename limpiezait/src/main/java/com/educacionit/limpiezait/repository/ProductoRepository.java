1ec96f558112029abca3ebad1dadbc95a9222691
francoaga <fandresgroselle@gmail.com>
2025-03-12 23:14:38 -0300

    Agregar anotacion @Repository e implemento metodos de la clase
    Se agrega la anotacion @Repository para que la clase tenga la responsabilidad de respositorio. Ademas, se completan las logicas de los metodos que tiene la clase.

--- limpiezait/src/main/java/com/educacionit/limpiezait/repository/ProductoRepository.java
+++ limpiezait/src/main/java/com/educacionit/limpiezait/repository/ProductoRepository.java
@@ -2,31 +2,53 @@ package com.educacionit.limpiezait.repository;
 
 import com.educacionit.limpiezait.model.Producto;
 import com.educacionit.limpiezait.repository.interfaces.IProductoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
+@Repository
 public class ProductoRepository implements IProductoRepository {
     private final List<Producto> productos = new ArrayList<>();
 
     @Override
     public List<Producto> findAll() {
-        return List.of();
+        return this.productos;
     }
 
     @Override
     public Optional<Producto> findBy(Long id) {
-        return Optional.empty();
+        return this.productos.stream()
+                .filter(producto -> Objects.equals(producto.getId(), id))
+                .findFirst();
     }
 
     @Override
-    public Producto save(Producto entity) {
-        return null;
+    public Producto save(Producto producto) {
+        Optional<Producto> productoOptional = findBy(producto.getId());
+
+        if (productoOptional.isPresent()) {
+            // Si el producto existe, lo editamos
+            edit(productoOptional.get(), producto);
+        } else {
+            // Si el producto no existe, lo agregamos a la lista
+            this.productos.add(producto);
+        }
+
+        return producto;
+    }
+
+    private void edit(Producto productoActual, Producto productoModificado) {
+        productoActual.setNombre(productoModificado.getNombre());
+        productoActual.setDescripcion(productoModificado.getDescripcion());
+        productoActual.setPrecio(productoModificado.getPrecio());
+        productoActual.setUrlFoto(productoModificado.getUrlFoto());
     }
 
     @Override
     public void delete(Long id) {
-
+        this.productos.removeIf(producto -> Objects.equals(producto.getId(), id));
     }
 }
