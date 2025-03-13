9851fecbff052f002d063ce21c508cb3aa309015
francoaga <fandresgroselle@gmail.com>
2025-03-12 23:09:14 -0300

    Agregar anotación @Builder y personalizar método builder
    Se agrega la anotación @Builder para implementar el patrón Builder
        en la clase Producto. Además, se personaliza el método builder
        para asegurar que el atributo id se autoincremente correctamente.

--- limpiezait/src/main/java/com/educacionit/limpiezait/model/Producto.java
+++ limpiezait/src/main/java/com/educacionit/limpiezait/model/Producto.java
@@ -1,10 +1,12 @@
 package com.educacionit.limpiezait.model;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 public class Producto {
     private static long idContador = 1;
     private Long id;
@@ -13,15 +15,16 @@ public class Producto {
     private String descripcion;
     private String urlFoto;
 
-    public Producto() {
-        this.id = idContador++;
-    }
-
-    public Producto(String nombre, Double precio, String descripcion, String urlFoto) {
-        this();
+    private Producto(Long id, String nombre, Double precio, String descripcion, String urlFoto) {
+        this.id = id;
         this.nombre = nombre;
         this.precio = precio;
         this.descripcion = descripcion;
         this.urlFoto = urlFoto;
+    }
+
+    // Método estático para obtener un Builder personalizado
+    public static ProductoBuilder builder() {
+        return new ProductoBuilder().id(idContador++);
     }
 }
