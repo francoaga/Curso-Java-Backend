cd1c701928e94101894750220ac9890d0c88c8b5
francoaga <fandresgroselle@gmail.com>
2025-03-12 23:27:13 -0300

    Crear interfaz IProductoMapper para mapear la entidad Producto a ProductoDTO y viceversa
    Se crea la interfaz IProductoMapper para que extienda de la interfaz generica IMapper y encapsule a la entidad de negocio Prodcuto y al dto ProductoDTO.

---
+++ limpiezait/src/main/java/com/educacionit/limpiezait/mapper/interfaces/IProductoMapper.java
@@ -0,0 +1,7 @@
+package com.educacionit.limpiezait.mapper.interfaces;
+
+import com.educacionit.limpiezait.dto.ProductoDTO;
+import com.educacionit.limpiezait.model.Producto;
+
+public interface IProductoMapper extends IMapper<ProductoDTO, Producto>{
+}
