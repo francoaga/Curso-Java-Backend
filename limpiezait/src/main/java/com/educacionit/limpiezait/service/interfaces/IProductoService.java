a4b1a34da1b5abf1d9f7371959cc5f2a7537ad2c
francoaga <fandresgroselle@gmail.com>
2025-03-12 23:34:57 -0300

    Crear interfaz IProductoService para operaciones de Producto
    Se crea la interfaz IProductoService, que extiende la interfaz genérica IService<ProductoDTO, Long>. Esta interfaz define las operaciones CRUD específicas para la entidad Producto, utilizando ProductoDTO como tipo de dato y Long como tipo de identificador.

---
+++ limpiezait/src/main/java/com/educacionit/limpiezait/service/interfaces/IProductoService.java
@@ -0,0 +1,7 @@
+package com.educacionit.limpiezait.service.interfaces;
+
+import com.educacionit.limpiezait.dto.ProductoDTO;
+
+public interface IProductoService extends IService <ProductoDTO, Long>{
+
+}
