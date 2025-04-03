1dc620f1373b67307fdde49391329a21b6fe69dc
francoaga <fandresgroselle@gmail.com>
2025-04-02 21:22:32 -0300

    feature(repository): añadir busqueda por nombre de producto case insensitive
    Se ha agregado el método findByNombreContainingIgnoreCase a la interfaz IProductoRepository para permitir busquedas de productos que contengan el texto indicado, se ignora mayusculas/minusculas.

--- limpiezait/src/main/java/com/educacionit/limpiezait/repository/interfaces/IProductoRepository.java
+++ limpiezait/src/main/java/com/educacionit/limpiezait/repository/interfaces/IProductoRepository.java
@@ -2,5 +2,8 @@ package com.educacionit.limpiezait.repository.interfaces;
 
 import com.educacionit.limpiezait.model.Producto;
 
+import java.util.List;
+
 public interface IProductoRepository extends IRepository<Producto, Long> {
+    List<Producto> findByNombreContainingIgnoreCase(String nombre);
 }
