d36c4c022ba1f665aeab0c7ffe7ed09b520b7181
francoaga <fandresgroselle@gmail.com>
2025-04-02 21:42:11 -0300

    refactor(exception-handler): cambiar @ControllerAdvice por @RestControllerAdvice
    Se actualizo la anotacion del manejador global de excepciones para mejorar la consistencia

--- limpiezait/src/main/java/com/educacionit/limpiezait/handler/GlobalExceptionHandler.java
+++ limpiezait/src/main/java/com/educacionit/limpiezait/handler/GlobalExceptionHandler.java
@@ -2,12 +2,12 @@ package com.educacionit.limpiezait.handler;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.NoSuchElementException;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(NoSuchElementException.class)
