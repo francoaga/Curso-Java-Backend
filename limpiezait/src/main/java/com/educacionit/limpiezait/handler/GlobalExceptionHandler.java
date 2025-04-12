878e62564e741d64f14e68984bbd26a0204a6ae8
francoaga <fandresgroselle@gmail.com>
2025-04-12 18:53:18 -0300

    Modificar implementacion metodos clase GlobalExceptionHandler
    Se modifica los metodos para que se puede utilizar la clase ErrorResponse para personalizar los errores

--- limpiezait/src/main/java/com/educacionit/limpiezait/handler/GlobalExceptionHandler.java
+++ limpiezait/src/main/java/com/educacionit/limpiezait/handler/GlobalExceptionHandler.java
@@ -5,18 +5,33 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import com.educacionit.limpiezait.handler.model.ErrorResponse;
+
+import java.time.LocalDate;
 import java.util.NoSuchElementException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(NoSuchElementException.class)
-    public ResponseEntity<String> handleNoSuchElementException(NoSuchElementException e) {
-        return new ResponseEntity<>("Recurso No Encontrado", HttpStatus.NOT_FOUND);
+    public ResponseEntity<ErrorResponse> handleNoSuchElementException(NoSuchElementException e) {
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .statusCode(HttpStatus.NOT_FOUND.value())
+                .timestamp(LocalDate.now())
+                .message(e.getMessage())
+                .build();
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.valueOf(errorResponse.getStatusCode()));
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleGenericException(Exception e) {
-        return new ResponseEntity<>("Error interno del servidor", HttpStatus.INTERNAL_SERVER_ERROR);
+    public ResponseEntity<ErrorResponse> handleGenericException(Exception e) {
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .statusCode(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .timestamp(LocalDate.now())
+                .message("Error interno del servidor")
+                .build();
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.valueOf(errorResponse.getStatusCode()));
     }
 }
